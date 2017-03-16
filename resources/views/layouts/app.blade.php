e59847e935aab74fb6dc60375a8d11098b8b3cc5
fsarachu <fsarachu@gmail.com>
2017-03-16 16:54:36 -0300

    Add favicons and theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,11 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicon.png">
+    <meta name="theme-color" content="#ff5456">
+    <meta name="msapplication-navbutton-color" content="#ff5456">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ff5456">
     <title>Rimbos - @yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
