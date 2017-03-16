b91ca9fc3d78744692c8ac9568a0188cb1af6a9f
fsarachu <fsarachu@gmail.com>
2017-03-16 16:25:43 -0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,11 @@
 <html>
 <head>
-    <title>App Name - @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>Rimbos - @yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-<div class="container">
+<div class="ui container">
     @yield('content')
 </div>
 <script src="/js/app.js"></script>
