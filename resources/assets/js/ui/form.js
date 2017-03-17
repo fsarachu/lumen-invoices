18dddc45a7cfc409cd6a55659e50de49ee9de134
fsarachu <fsarachu@gmail.com>
2017-03-17 14:22:26 -0300

    Install and setup semantic-ui-calendar

--- resources/assets/js/ui/form.js
+++ resources/assets/js/ui/form.js
@@ -1,6 +1,33 @@
 import '../../semantic/src/definitions/modules/transition';
 import '../../semantic/src/definitions/modules/dropdown';
 import '../../semantic/src/definitions/modules/checkbox';
+import '../../semantic/src/definitions/modules/popup';
+import '../../../../node_modules/semantic-ui-calendar/src/definitions/modules/calendar'
 
 $('.dropdown.selection').dropdown();
 $('.ui.checkbox').checkbox();
+
+let calendarOptions = {
+    type: 'date',
+    formatter: {
+        date: formatDate
+    }
+};
+
+$('#calendar').calendar(calendarOptions).calendar('set date', new Date());
+
+function zeroFill(number, width) {
+    width -= number.toString().length;
+    if (width > 0) {
+        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+    }
+    return number + ""; // always return a string
+}
+
+function formatDate(date) {
+    if (!date) return '';
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    return year + '-' + zeroFill(month, 2) + '-' + zeroFill(day, 2);
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -28,5 +28,9 @@ mix.webpackConfig({
 });
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .styles(['resources/assets/semantic/dist/semantic.css', 'resources/assets/css/custom.css'], 'public/css/app.css')
+    .styles([
+        'resources/assets/semantic/dist/semantic.css',
+        'node_modules/semantic-ui-calendar/dist/calendar.css',
+        'resources/assets/css/custom.css'
+    ], 'public/css/app.css')
     .copy('node_modules/semantic-ui/src/themes/default/assets/fonts', 'public/fonts');
