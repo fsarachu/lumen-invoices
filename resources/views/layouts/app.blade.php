ebfb453b871626b057837689bd9c684736dad7c2
fsarachu <fsarachu@gmail.com>
2017-03-17 16:55:57 -0300

    Disable user-scaling, fixes iOS annoying input zooming

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <html lang="es">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" href="favicon.png">
     <meta name="theme-color" content="#ff5456">
