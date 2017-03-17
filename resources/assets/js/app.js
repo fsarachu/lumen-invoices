d465d5b97994ee7b9c927a887f28fb3e1abce791
fsarachu <fsarachu@gmail.com>
2017-03-16 23:30:45 -0300

    Add some styling and enable form components js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,12 @@
-import '../semantic/src/definitions/modules/sidebar.js';
+import '../semantic/src/definitions/modules/sidebar';
+import '../semantic/src/definitions/modules/transition';
+import '../semantic/src/definitions/modules/dropdown';
+import '../semantic/src/definitions/modules/checkbox';
 
 $('#sidebar-toggle').on('click', () => {
     $('#sidebar-menu').sidebar('toggle');
 });
+
+$('.dropdown.selection').dropdown();
+
+$('.ui.checkbox').checkbox();
