14e49fdcc5a3c67881895ba8d54492359d3fc909
fsarachu <fsarachu@gmail.com>
2017-03-17 02:32:15 -0300

    Refactor js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,1 @@
-import '../semantic/src/definitions/modules/sidebar';
-import '../semantic/src/definitions/modules/transition';
-import '../semantic/src/definitions/modules/dropdown';
-import '../semantic/src/definitions/modules/checkbox';
-
-$('#sidebar-toggle').on('click', () => {
-    $('#sidebar-menu').sidebar('toggle');
-});
-
-$('.dropdown.selection').dropdown();
-
-$('.ui.checkbox').checkbox();
+import './ui/_all';
---
+++ resources/assets/js/ui/form.js
@@ -0,0 +1,6 @@
+import '../../semantic/src/definitions/modules/transition';
+import '../../semantic/src/definitions/modules/dropdown';
+import '../../semantic/src/definitions/modules/checkbox';
+
+$('.dropdown.selection').dropdown();
+$('.ui.checkbox').checkbox();
