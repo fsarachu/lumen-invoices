52a05d211c337d43785a823325bee720ddf09b8e
fsarachu <fsarachu@gmail.com>
2017-03-18 10:06:00 -0300

    Make migration "create_invoices_table"

---
+++ database/migrations/2017_03_18_124156_create_invoices_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->string('trip');
+            $table->integer('country_id');
+            $table->text('description');
+            $table->string('business_name');
+            $table->string('invoice_number');
+            $table->integer('category_id');
+            $table->integer('payment_method_id');
+            $table->integer('currency_id');
+            $table->decimal('amount_in_original_currency');
+            $table->decimal('one_dollar_rate');
+            $table->decimal('amount_in_dollars');
+            $table->decimal('actual_paid_amount');
+            $table->string('image_url');
+            $table->boolean('include_rut');
+            $table->boolean('assign_anii');
+            $table->boolean('personal_spending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
