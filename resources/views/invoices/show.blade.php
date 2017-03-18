6bc908ed0156f315d10e9f9f9a59fb91299e4596
fsarachu <fsarachu@gmail.com>
2017-03-18 14:30:01 -0300

    Basic show view

---
+++ resources/views/invoices/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Comprobantes')
+
+@section('content')
+    <div class="ui container">
+        <div class="ui one column wide grid">
+            <div class="column">
+                <h3 class="ui horizontal header divider">Comprobante Nro. {{$invoice->id}}</h3>
+            </div>
+            <div class="column">
+                <pre>{{ json_encode($invoice, JSON_PRETTY_PRINT) }}</pre>
+            </div>
+        </div>
+    </div>
+@endsection
