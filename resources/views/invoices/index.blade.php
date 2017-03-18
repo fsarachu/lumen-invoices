d10ee1e861933c817c31505b4d3fd62a74e1fa92
fsarachu <fsarachu@gmail.com>
2017-03-18 13:46:04 -0300

    Basic index view

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -3,5 +3,34 @@
 @section('title', 'Comprobantes')
 
 @section('content')
-    <h1>All Invoices</h1>
+    <div class="ui container">
+        <div class="ui one column wide grid">
+            <div class="column">
+                <h3 class="ui horizontal header divider">Listado de Comprobantes</h3>
+            </div>
+
+            <div class="column">
+                <table class="ui unstackable selectable celled table">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Fecha</th>
+                        <th class="right aligned">Moneda</th>
+                        <th class="right aligned">Monto</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($invoices as $invoice)
+                        <tr>
+                            <td>{{ $invoice->id }}</td>
+                            <td>{{ $invoice->date }}</td>
+                            <td class="right aligned">{{ $invoice->currency_id }}</td>
+                            <td class="right aligned">{{ $invoice->amount_in_original_currency }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
