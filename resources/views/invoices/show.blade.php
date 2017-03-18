86eb14174de9b95397df1348da5b0b778b3dc505
fsarachu <fsarachu@gmail.com>
2017-03-18 17:55:19 -0300

    Unescaped slashes in json_encode

--- resources/views/invoices/show.blade.php
+++ resources/views/invoices/show.blade.php
@@ -9,7 +9,7 @@
                 <h3 class="ui horizontal header divider">Comprobante Nro. {{$invoice->id}}</h3>
             </div>
             <div class="column">
-                <pre>{{ json_encode($invoice, JSON_PRETTY_PRINT) }}</pre>
+                <pre>{{ json_encode($invoice, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES) }} </pre>
             </div>
         </div>
     </div>
