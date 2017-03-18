bfff0824a9730d0731d70851d7bfad7886c39c3f
fsarachu <fsarachu@gmail.com>
2017-03-17 23:21:28 -0300

    Make migration create_currencies_table

---
+++ database/migrations/2017_03_18_011237_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('2_letter_code');
+            $table->string('3_letter_code');
+            $table->string('symbol');
+            $table->decimal('last_one_dollar_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
