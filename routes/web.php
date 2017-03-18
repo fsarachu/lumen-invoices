10f4b2f288e25293e4ae6a6eb3713bb4a7579388
fsarachu <fsarachu@gmail.com>
2017-03-18 16:48:36 -0300

    Remove test route

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,3 @@ $app->get('/invoices/{id:\d+}', 'InvoiceController@show');
 $app->get('/invoices/new', 'InvoiceController@create');
 
 $app->post('/invoices', 'InvoiceController@store');
-
-$app->get('/currencies', function () {
-    return DB::table('currencies')->get();
-});
