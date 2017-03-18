1f6df74e7101031c628d3788abdf52eee3f47bbf
fsarachu <fsarachu@gmail.com>
2017-03-18 11:21:21 -0300

    Add PaymentMethodsTableSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call('CurrenciesTableSeeder');
         $this->call('CountriesTableSeeder');
+        $this->call('PaymentMethodsTableSeeder');
     }
 }
---
+++ database/seeds/PaymentMethodsTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PaymentMethodsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Efectivo (Moneda Local)'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Efectivo (U$S)'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de crédito MD'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de crédito MD LUPS'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de débito ITAU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Banco BROU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Banco ITAU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Paypal'
+        ]);
+
+    }
+}
