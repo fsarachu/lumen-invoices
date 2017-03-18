a50cc3c2ec9dcb8695f3c52d1bf6142f26fe21bf
fsarachu <fsarachu@gmail.com>
2017-03-18 18:49:07 -0300

    Fix favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="favicon.png">
     <meta name="theme-color" content="#ff5456">
     <meta name="msapplication-navbutton-color" content="#ff5456">
     <meta name="apple-mobile-web-app-status-bar-style" content="#ff5456">
