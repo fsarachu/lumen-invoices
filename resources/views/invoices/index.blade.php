6eccd966d7cdb9724f513bdbb93b6067d3d521cf
fsarachu <fsarachu@gmail.com>
2017-03-19 01:18:22 -0300

    Improve invoice listing on mobile

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -10,12 +10,12 @@
             </div>
 
             <div class="column">
-                <table class="ui unstackable celled table">
+                <table class="ui unstackable single line basic table">
                     <thead>
                     <tr>
                         <th>Id</th>
                         <th>Fecha</th>
-                        <th class="right aligned">Moneda</th>
+                        <th>Moneda</th>
                         <th class="right aligned">Monto</th>
                     </tr>
                     </thead>
@@ -23,9 +23,15 @@
                     @foreach($invoices as $invoice)
                         <tr>
                             <td class="selectable"><a href="/invoices/{{ $invoice->id }}">{{ $invoice->id }}</a></td>
-                            <td>{{ $invoice->date }}</td>
-                            <td class="right aligned">{{ $invoice->currency_id }}</td>
-                            <td class="right aligned">{{ $invoice->amount_in_original_currency }}</td>
+                            <td>{{ Carbon\Carbon::parse($invoice->date)->format('d/m/y') }}</td>
+                            <td>
+                                @if($invoice->has('currency'))
+                                    <i class="{{$invoice->currency->iso_2}} flag"></i> {{ $invoice->currency->iso_3 }}
+                                @else
+                                    -
+                                @endif
+                            </td>
+                            <td class="right aligned">{{$invoice->currency->symbol}} {{ $invoice->amount_in_original_currency }}</td>
                         </tr>
                     @endforeach
                     </tbody>
